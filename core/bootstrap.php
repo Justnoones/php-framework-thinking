3a85217e5f857f7cbbad77ae10604d0ecfdd8290
Justnoones <yanw97841@example.com>
2023-12-13 08:24:45 +0630

    file-autoload

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -6,5 +6,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->load(__DIR__ . '.env');
 
-require "core/helperFunctions.php";
 App::bind("database", new QueryBuilder(Connection::make()));
